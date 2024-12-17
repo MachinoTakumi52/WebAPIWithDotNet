1fd61d6a3740f76f859e31cbbb4fddaa25da0980
lil4ty6ix <107854172+MachinoTakumi52@users.noreply.github.com>
2024-12-17 11:40:27 +0900

    Add files via upload

---
+++ .createProject/template/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using webApiProJect;
+
+/// <summary>
+/// 例外処理コントローラー
+/// </summary>
+[Route($"api/{buildsettings.version}/[controller]")]
+[ApiController]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorController : ControllerBase
+{
+    /// <summary>
+    /// 例外処理API　例外が起きたときはここに来る
+    /// </summary>
+    /// <returns></returns>
+    [Route("/error")]
+    public JsonResult Error()
+    {
+        //コンテキスト取得
+        var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+        //例外取得
+        var exception = context!.Error;
+        //エラーメッセージ設定
+        var response = new { error = exception.Message };
+        return new JsonResult(response);
+    }
+}
---
+++ .createProject/template/SampleController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using webApiProJect;
+
+
+/// <summary>
+/// サンプルコントローラー
+/// </summary>
+[ApiController] //APIコントローラーを示す属性
+[Route($"api/{buildsettings.version}/[controller]/[action]")] //ルーティングの設定
+public class SampleController : ControllerBase //コントローラークラスはControllerBaseを継承する必要あり
+{
+    /// <summary>
+    /// 受け取った文字にHelloを付けて返却
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    [HttpGet] //HTTPのGETメソッドを示す属性
+    //「api/buildsettings.version/Sample/Get」 にアクセスされたときに呼び出される
+    // [FromQuery]を使用することで、クエリパラメータを受け取ることができる
+    public JsonResult Get([FromQuery] string text)
+    {
+        return new JsonResult("Hello " + text);
+    }
+
+    /// <summary>
+    /// 受け取った文字にHelloを付けて返却
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    [HttpPost]//HTTPのPOSTメソッドを示す属性
+    //「api/buildsettings.version/Sample/Post」 にアクセスされたときに呼び出される
+    // [FromBody]を使用することで、リクエストボディを受け取ることができる
+    public JsonResult Post([FromBody] SampleModel sample)
+    {
+        return new JsonResult("Hello " + sample.Name);
+    }
+
+    /// <summary>
+    /// 受け取った文字にHelloを付けて返却
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    [HttpPut] //HTTPのPUTメソッドを示す属性
+    //「api/buildsettings.version/Sample/Put」 にアクセスされたときに呼び出される
+    // [FromBody]を使用することで、クエリパラメータを受け取ることができる
+    public JsonResult Put([FromBody] string text)
+    {
+        return new JsonResult("Hello " + text);
+    }
+
+    /// <summary>
+    /// 受け取ったIDに対応するデータを削除
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete] //HTTPのDELETEメソッドを示す属性
+    //「api/buildsettings.version/Sample/Delete」 にアクセスされたときに呼び出される
+    // [FromBody]を使用することで、クエリパラメータを受け取ることができる
+    public JsonResult Delete([FromBody] int id)
+    {
+        return new JsonResult("Deleted data with ID: " + id);
+    }
+
+}
